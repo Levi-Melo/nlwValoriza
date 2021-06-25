efe3592c115ee9929c4ddbdf456da2f553c7b3aa
levi melo <levicontat@outlook.com>
2021-06-25 12:29:32 -0300

    adding password to users

--- src/services/CreateUserService.ts
+++ src/services/CreateUserService.ts
@@ -1,14 +1,15 @@
 import { UsersRepositories } from "../repositories/UsersRepositories";
 import { getCustomRepository } from "typeorm";
-
+import { hash } from "bcryptjs";
 interface IUserRequest {
   name: string;
   email: string;
   admin?: boolean;
+  password: string;
 }
 
 export class CreateUserService {
-  async execute({ name, email, admin }: IUserRequest) {
+  async execute({ name, email, admin, password }: IUserRequest) {
     const usersRepositories = getCustomRepository(UsersRepositories);
     const userAlreadyExists = await usersRepositories.findOne({ email });
 
@@ -16,11 +17,18 @@ export class CreateUserService {
       throw new Error("E-mail incorrect");
     }
 
+    const passwordHash = await hash(password, 8);
+
     if (userAlreadyExists) {
       throw new Error("User already exists");
     }
 
-    const user = usersRepositories.create({ name, email, admin });
+    const user = usersRepositories.create({
+      name,
+      email,
+      admin,
+      password: passwordHash,
+    });
     await usersRepositories.save(user);
     return user;
   }
